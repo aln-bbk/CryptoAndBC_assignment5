a69c3332a3b96fa57e937cf9be6ca14a25ba1980
aln-bbk <Alina.babkevich@gmail.com>
2026-04-06 15:04:33 +0300

    Translate comments in hardhat.config.js to English

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
-// 1. Подключаем работу с файлом .env
+// 1. Enable environment variable management via the .env file
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -7,9 +7,9 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      // Публичная ссылка для подключения к сети
+      // Public RPC URL for connecting to the network
       url: "https://ethereum-sepolia-rpc.publicnode.com",
-      // Берем ключ из .env (переменная PRIVATE_KEY)
+      // Retrieve the private key from .env (PRIVATE_KEY variable)
       accounts: [process.env.PRIVATE_KEY]
     }
   }
